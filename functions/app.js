e844b839ffb96550d7549333e5532d9894e4242c
vmhowley <vmhowleyh@gmail.com>
2024-10-16 18:22:32 -0400

    last

--- functions/app.js
+++ functions/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const serverless = require("serverless-http");
 
 const mongoose = require('mongoose');
-const Producto = require('../models/Producto');
+const Producto = require('./models/Producto');
 const cors = require('cors');
 const app = express();
+const router = express.Router();
 module.exports.handler = serverless(app);
 app.use(cors());
 // Exportar el handler para ser usado con Serverless Framework
@@ -12,7 +13,7 @@ app.use(cors());
 const PORT = 3000;
 
 // Middleware para poder recibir JSON en las solicitudes
-app.use(express.json());
+router.use(express.json());
 
 // Conexión a MongoDB
 mongoose.connect('mongodb://localhost:27017/sneackculture', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -20,16 +21,16 @@ mongoose.connect('mongodb://localhost:27017/sneackculture', { useNewUrlParser: t
     .catch(err => console.error('Error al conectar con MongoDB', err));
 
 // Ruta simple de prueba
-app.get('/', (req, res) => {
+router.get('/', (req, res) => {
     res.send('Bienvenido a mi aplicación web');
 });
 
 // Inicia el servidor
-app.listen(PORT, () => {
+router.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
 // Ruta para obtener todos los productos
-app.get('/productos', async (req, res) => {
+router.get('/productos', async (req, res) => {
     try {
         const productos = await Producto.find();
         res.json(productos);
@@ -39,7 +40,7 @@ app.get('/productos', async (req, res) => {
 });
 
 // Ruta para añadir un nuevo producto
-app.post('/producto', async (req, res) => {
+router.post('/producto', async (req, res) => {
     const { nombre, precio, stock } = req.body;
 
     const nuevoProducto = new Producto({ nombre, precio, stock });
