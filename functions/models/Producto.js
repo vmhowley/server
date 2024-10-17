26ff6ad26b9c6880f611789d7359feceec26fdb6
vmhowley <vmhowleyh@gmail.com>
2024-10-17 13:36:59 -0400

    DataBase

--- functions/models/Producto.js
+++ functions/models/Producto.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const productoSchema = new mongoose.Schema({
     name:       { type: String, required: true},
     brand:      { type: String, required: true},
-    imagen:     { type: String, required: true},
+    image:     { type: String, required: true},
     size:       { type: String, required: true},
     price:      { type: String, required: true},
     quantity:   { type: String, required: true},
