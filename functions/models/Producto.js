1d786a3146b97c74c430b08029d6e84373cb2f5d
vmhowley <vmhowleyh@gmail.com>
2024-10-17 19:13:57 -0400

    Last

--- functions/models/Producto.js
+++ functions/models/Producto.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const productoSchema = new mongoose.Schema({
-    name:       { type: String, required: true},
+    name:       { type: String, required: true },
     brand:      { type: String, required: true},
     image:     { type: String, required: true},
     size:       { type: String, required: true},
-    price:      { type: String, required: true},
-    quantity:   { type: String, required: true},
+    price:      { type: Number, required: true},
+    quantity:   { type: Number, required: true},
     category:   { type: String, required: true},
     condition:  { type: String, required: true},
     
